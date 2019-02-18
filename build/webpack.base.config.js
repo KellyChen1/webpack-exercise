005b4aacdf673af992a84cd8d1fd92e9abb0e3e6
chenlujia <lujia.chen@aihuishou.com>
2019-02-18 18:32:44 +0800

    1. 增加样式loader, style-loader, css-loader, less-loader, postcss-loader, autoprefixed 2. css单独打包成一个文件，extract-text-webpack-plugin,.

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack')
 const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin')  //在html动态插入打包的js
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
   entry: {
@@ -10,34 +11,57 @@ module.exports = {
     "app": [path.resolve(__dirname, "../src/index.js")],  //不懂
     "common": ["babel-polyfill"]      //
   },
-  output:{
-    path: path.resolve(__dirname,"../dist"),
-    filename:"[name]-[chunkhash:8].js",
+  output: {
+    path: path.resolve(__dirname, "../dist"),
+    filename: "[name]-[chunkhash:8].js",
   },
-  module:{
+  module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,      //不对node_modules进行编译，提升打包速度
         loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use:"css-loader"
+        })
+      },
+      {
+        test: /\.less$/,
+        use: ExtractTextPlugin.extract({
+          use:[                    //use: 用loader去编译
+            { loader: "css-loader" },
+            { loader: "postcss-loader" },
+            { loader: "less-loader"}
+          ],
+          fallback:"style-loader"   //fallback用style-loader提取css
+        })
       }
     ]
   },
-  plugins:[
+  plugins: [
     new htmlWebpackPlugin({
       title: 'best kelly',    //生成html的title属性
       filename: 'index.html',      //生成到dist目录html的文件名
-      file:{ 
+      file: {
         "js": ["app.js", "common.js"]
       },
       template: path.resolve(__dirname, "../src/index.html"),  //入口模板
       favicon: path.resolve(__dirname, "../src/favicon.ico"), //加载网站图标
       inject: "body",  //true === body, js加在body里
-      minify:{
+      minify: {
         removeComments: true,   //移除注释
         collapseWhitespace: true, //移除空白
         removeAttributeQuotes: true,  //移除html中可以去掉的引号
       }
+    }),
+
+    new ExtractTextPlugin({
+      filename: "./style.css",      //extract-text-webpack-plugin 在webpack4不能用hash
+      allChunks: false
     })
 
   ]
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import "./app.less"
 export default class App extends Component {
   constructor(props) {
     super(props);
